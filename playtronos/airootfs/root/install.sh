a302d8eb47fcff6438a455200ba02094831ccb9f
Alesh Slovak <aleshslovak@gmail.com>
2026-04-07 10:45:02 -0400

    update disk image size for improved progress estimate

--- playtronos/airootfs/root/install.sh
+++ playtronos/airootfs/root/install.sh
@@ -8,7 +8,7 @@ fi
 
 OS_NAME="Playtron GameOS"
 MIN_DISK_SIZE=55 # GB
-RAW_DISK_SIZE=9 # size in GB of the uncompressed disk image; used for estimating progress
+RAW_DISK_SIZE=13 # size in GB of the uncompressed disk image; used for estimating progress
 
 DEVICE_VENDOR=$(cat /sys/devices/virtual/dmi/id/sys_vendor)
 DEVICE_PRODUCT=$(cat /sys/devices/virtual/dmi/id/product_name)
